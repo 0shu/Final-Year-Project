eb77f29ad4035cfbd78b68a8024f2142747deaab
Joshua Simons <joshua.simons.contact@gmail.com>
2021-03-13 03:07:30 +0000

    Added compression function
    Compresses the vertices vertically along the compression normal towards the center

--- Classes/Mesh-Vertex.cs
+++ Classes/Mesh-Vertex.cs
@@ -9,11 +9,14 @@ public class Mesh_Vertex
     public float m_affect = 0.0f;
 
     public void TransferForce(Vector force, float power) {
+        //Subtract temp absorbtion
         m_affect += power - (/* Some amount calc through temp, hardness etc*/);
 
+        //Check vertex is not next to immovable and force is in direction of said immovable
+
+        //Go through conected points and tranfer force where appropriate
         list<pair<float, Mesh_Vertex>> valid;
         float totalAngle;
-
         foreach(vert in m_connected) {
             Vector diff = vert - m_position;
             float angle = Vector.angleBetween(force, diff);
@@ -22,10 +25,16 @@ public class Mesh_Vertex
                 totalAngle += angle;
             }
         }
-
         foreach(vert in valid) {
             vert.second.TransferForce(force, m_affect * (vert.first / totalAngle));
         }
+    }
+
+    public void Compress(Vector normal, Vector center) {
+        Vector direction; //Check if above or below center with relation to normal
+        float compression = m_affect * m_material.hardness;
+        direction *= compression;
+        m_position += direction;
     }
 
     public void ResetAffect() {
