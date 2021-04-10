eb93ad2319833f6009849b35d6529b56f17c295a
Joshua Simons <joshua.simons.contact@gmail.com>
2021-04-10 22:14:07 +0100

    more dsumb work on compute shader, still broken

--- Unity/Final Year Project/Assets/Scripts/volumeFind.cs
+++ Unity/Final Year Project/Assets/Scripts/volumeFind.cs
@@ -13,24 +13,20 @@ public struct Tetra
 public class volumeFind : MonoBehaviour
 {
     public ComputeShader m_compute;
-    public List<Tetra> m_tetras = new List<Tetra>();
     public Mesh m_mesh;
     public Vector3 m_origin = new Vector3();
-    public float[] m_vols = new float[20];
     public float m_total = 0;
     public float m_arraysVol = 0;
     public int m_faces = 0;
     // Start is called before the first frame update
     void Start()
     {
-        GetMeshAsTetras();
-        VolOnGPU();
+
     }
 
     // Update is called once per frame
     void Update()
     {    
-        //VolOnGPU();
         VolWithArrays();
     }
 
@@ -43,6 +39,7 @@ public class volumeFind : MonoBehaviour
         Debug.Log("Verts: " + verts.Length);
         Debug.Log("Tris: " + indices.Length);
 
+        //int amt = Mathf.CeilToInt(indices.Length / 3.0f);
         float[] volumes = new float[indices.Length];
         for (int i = 0; i < volumes.Length; i++)
         {
@@ -59,7 +56,7 @@ public class volumeFind : MonoBehaviour
         float[] origin = {m_origin.x, m_origin.y, m_origin.z};
         m_compute.SetFloats("origin", origin);
         m_compute.SetBuffer(0, "positions", vertBuffer);
-        m_compute.SetBuffer(0, "indices", vertBuffer);
+        m_compute.SetBuffer(0, "indices", indiceBuffer);
         m_compute.SetBuffer(0, "volumes", volumeBuffer);
         m_compute.Dispatch(0, Mathf.CeilToInt(indices.Length / 30.0f), 1, 1);
 
@@ -72,64 +69,9 @@ public class volumeFind : MonoBehaviour
             m_arraysVol += volumes[i];
         }
 
+        Debug.Log("Finished Calculating Volume! " + m_arraysVol);
         vertBuffer.Dispose();
         indiceBuffer.Dispose();
         volumeBuffer.Dispose();
-        Debug.Log("Finished Calculating Volume! " + m_arraysVol);
-    }
-
-    public void VolOnGPU()
-    {
-        Debug.Log("Starting to calc volume!");
-        int totalSize = sizeof(float) * 10;
-        ComputeBuffer tetraBuffer = new ComputeBuffer(m_tetras.Count, totalSize);
-        tetraBuffer.SetData(m_tetras.ToArray());
-
-        float[] origin = {m_origin.x, m_origin.y, m_origin.z};
-        m_compute.SetBuffer(0, "tetras", tetraBuffer);
-        m_compute.SetFloats("origin", origin);
-        m_compute.Dispatch(0, Mathf.CeilToInt(m_tetras.Count / 10.0f), 1, 1);
-
-        Tetra[] newTetras = new Tetra[m_faces];
-        tetraBuffer.GetData(newTetras);
-
-
-        m_total = 0.0f;
-        for (int i = 0; i < newTetras.Length; i++)
-        {
-            m_total += newTetras[i].vol;
-            m_vols[i] = newTetras[i].vol;
-            newTetras[i].vol = 0;
-        }
-        m_tetras = new List<Tetra>(newTetras);
-
-        tetraBuffer.Dispose();
-        Debug.Log("Finished Calculating Volume!");
-    }
-
-    public void GetMeshAsTetras()
-    {
-        Debug.Log("Starting to get mesh!");
-        Vector3[] verts = m_mesh.vertices;
-        int[] indices = m_mesh.triangles;
-
-        Debug.Log("Verts: " + verts.Length);
-        Debug.Log("Tris: " + indices.Length);
-
-        if( m_tetras.Count > 0) m_tetras.RemoveRange(0, m_tetras.Count);
-
-        for(int i = 0; i <= indices.Length - 3; i+=3)
-        {
-            Tetra tetra;
-            tetra.a = verts[indices[i]];
-            tetra.b = verts[indices[i+1]];
-            tetra.c = verts[indices[i+2]];
-            tetra.vol = 0.0f;
-
-            m_tetras.Add(tetra);
-        }
-        Debug.Log("Finished getting mesh!");
-
-        m_faces = m_tetras.Count;
     }
 }
