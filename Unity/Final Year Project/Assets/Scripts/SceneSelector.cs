84dd2e69aa39aac6749e9b0f1447dab8da078e47
Joshua Simons <joshua.simons.contact@gmail.com>
2021-04-13 20:46:30 +0100

    extra scenes and selector script for navigating

---
+++ Unity/Final Year Project/Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SceneSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string name)
+    {
+        Debug.Log("Loading Scene: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        if(EditorApplication.isPlaying) 
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+ #endif
+
+        Debug.Log("Closing Game!");
+        Application.Quit();
+    }
+}
